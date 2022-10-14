18a8944a88de0afd847b567a12ae003ae369852b
Benjamin <bmwoodforde@gmail.com>
2022-10-14 13:42:00 +1100

    implemented a responsive carousel but still having bugs on page refresh

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,16 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+
 import axios from 'axios';
+
+import OwlCarousel, { options } from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+// Owl Carousel: Very code light with many options
+// Keen Slider: Appears to be more mobile focused and to get an appearance that matches the spec requires too much code
+// React Slick: Appears to be a good option with many options
+
+// Both owl and slick appear to be very good options, but I find the owl documentation easier to read when looking for carousel options
 
 const HotCollections = () => {
 
   const [collections, setCollections] = useState([]);
+  // const [loaded, setLoaded] = useState(true);
+
+  const carouselOptions = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    // lazyLoading: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      572: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    }
+  }
 
   async function getCollections() {
     let { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`);
     setCollections(data);
+    // setLoaded(true);
   }
 
   useEffect(() => {
@@ -27,10 +60,12 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
+          <OwlCarousel className="owl-theme" {...carouselOptions}>
           {
-            collections.slice(0, 4).map((collection) => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={collection.id}>
-                <div className="nft_coll">
+            // loaded &&
+            collections.map((collection) => (
+              // <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={collection.id}>
+                <div className="nft_coll" key={collection.id}>
                   <div className="nft_wrap">
                     <Link to={`/item-details/${collection.nftId}`}>
                       <img src={collection.nftImage} className="lazy img-fluid" alt="" />
@@ -49,9 +84,10 @@ const HotCollections = () => {
                     <span>ERC-{collection.code}</span>
                   </div>
                 </div>
-              </div>
+              // </div>
             ))
           }
+          </OwlCarousel>
           {/* {new Array(4).fill(0).map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
